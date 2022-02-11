f9b4d22d7f5ae51d43cd9cad367e93e7b5392505
yugmani <yugmani@yahoo.com>
2022-02-10 22:46:57 -0800

    some more codes of oop for practice

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>Look at the console</h1>`;
 
+// 1
 // Class Declarations
 class Rectangle {
   constructor(height, width) {
@@ -29,4 +30,33 @@ console.log(`Area of square is: ${square.area} sq units.`);
 
 //instance of a rectangle
 const rectangle = new Rectangle(20, 30);
-console.log(`Area of rectangle is: ${rectangle.area} sq units.`);
+// console.log(`Area of rectangle is: ${rectangle.area} sq units.`);
+
+// 2
+const person = {
+  name: 'Bob Smith',
+  interest: ['music', 'dance'],
+  greet: function () {
+    return `Welcome ${this.name}`;
+  },
+};
+
+// console.log(person.name);
+// console.log(person.interest[0]);
+// console.log(person.greet());
+
+// 3 
+function Person(name) {
+  this.name = name;
+
+  this.greeting = function(){
+    alert("HI! I\'m " + this.name + '.')
+  }
+}
+
+let person1 = new Person('Bob');
+
+// console.log(person1);
+// console.log(person1.name);
+// console.log(person1.greeting());
+
