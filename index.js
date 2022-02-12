b008092a809817c7c01bc56300f42cbeef572fc3
yugmani <yugmani@yahoo.com>
2022-02-11 22:43:47 -0800

    inheritance

--- index.js
+++ index.js
@@ -26,13 +26,14 @@ class Rectangle {
 
 // instance of a square
 const square = new Rectangle(20, 20);
-console.log(`Area of square is: ${square.area} sq units.`);
+// console.log(`Area of square is: ${square.area} sq units.`);
 
 //instance of a rectangle
 const rectangle = new Rectangle(20, 30);
 // console.log(`Area of rectangle is: ${rectangle.area} sq units.`);
 
 // 2
+// Method: Special Property of Object
 const person = {
   name: 'Bob Smith',
   interest: ['music', 'dance'],
@@ -45,13 +46,14 @@ const person = {
 // console.log(person.interest[0]);
 // console.log(person.greet());
 
-// 3 
+// 3
+// Constructor Function
 function Person(name) {
   this.name = name;
 
-  this.greeting = function(){
-    alert("HI! I\'m " + this.name + '.')
-  }
+  this.greeting = function () {
+    console.log("HI! I'm " + this.name + '.');
+  };
 }
 
 let person1 = new Person('Bob');
@@ -60,3 +62,89 @@ let person1 = new Person('Bob');
 // console.log(person1.name);
 // console.log(person1.greeting());
 
+// 4
+// Create objects using `create()`
+let person2 = Object.create(person1);
+person2.name = 'dob';
+// console.log(person2.greeting());
+// console.log(person2);
+
+//5
+// Verify an Object's Constructor with instanceof
+// console.log(person2 instanceof Person);
+
+// 6
+// Own Properties
+function User(name, age, status) {
+  this.name = name;
+  this.age = age;
+  this.status = status;
+  this.designation = function () {
+    console.log(`Hello! I\'m ${this.name} & work as ${this.status}.`);
+  };
+}
+
+const user1 = new User('Irene', 27, 'engineer');
+
+// hasOwnProperty()
+let myProps = [];
+for (let property in user1) {
+  if (user1.hasOwnProperty(property)) {
+    myProps.push(property);
+  }
+}
+
+// console.log(myProps);
+
+// 7
+// Prototypes
+User.prototype = {
+  constructor: User,
+  phone: '310',
+
+  performance: function () {
+    console.log('Excellent');
+  },
+};
+
+const user2 = new User('Moona', 21, 'Receptionist');
+
+// console.log(user1);
+// console.log(User.prototype.isPrototypeOf(user2)); // true
+// console.log(User.prototype.isPrototypeOf(user1)); // false
+// console.log(user1.hasOwnProperty('phone')); // false
+// console.log(user2.hasOwnProperty('phone')); // false
+
+// 8
+//INHERITANCE
+function Cat(name) {
+  this.name = name;
+}
+
+Cat.prototype = {
+  constructor: Cat,
+  //   eat: function () {   //DRY
+  //     console.log('nom nom nom');
+  //   },
+};
+
+function Bear(name) {
+  this.name = name;
+}
+
+Bear.prototype = {
+  constructor: Bear,
+  // eat: function () { //DRY
+  //   console.log('nom nom nom');
+  // },
+};
+
+function Animal() {}
+
+Animal.prototype = {
+  constructor: Animal,
+
+  eat: function () {
+    console.log('nom nom nom');
+  },
+};
